d4a7cc495a63221bc6c1ec9b6b66d08c2085e7fb
Ibrahim1249 <mominibrahim98.im85@gmail.com>
2024-06-11 19:06:41 +0530

    completed progress bar assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,35 @@
+import React from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
+function App() {
+  const [progress, setProgress] = useState(0);
+  const [status, setStatus] = useState("Loading");
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress >= 100) {
+          setStatus("Complete");
+          clearInterval(interval);
+          return 100;
+        }
+        return prevProgress + 1;
+      });
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <>
+      <div className="progress-container">
+        <h1>Progress Bar</h1>
+        <div className="progress">
+          <div className="bar" style={{width : `${progress}%`}}></div>
+        </div>
+        <p>{progress} %   {status}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
